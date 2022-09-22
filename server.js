d5c6f6f6c49f6094b0a4524b5243a8bb01df9d36
aladynjr <aladynjr@gmail.com>
2022-09-21 23:00:12 -0700

    fix bugs

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const RegisterVehicle = (
                 '--no-sandbox',
                 '--disable-gpu',
                 '--enable-webgl',
-                '--window-size=800,800'
+                '--window-size=1860,1400'
             ]
         });
 
@@ -90,64 +90,86 @@ const RegisterVehicle = (
 
         await page.setUserAgent(ua);
         await page.goto(submissionUrl, { waitUntil: 'networkidle2' });
+       
         await page.waitForSelector('body > main > section:nth-child(2) > nav:nth-child(1) > a:nth-child(5)');
         await clickByText(page, `register vehicle`);
         await page.waitForTimeout(4000)
+
+      //  const data = await page.evaluate(() => document.querySelector('valid').outerHTML);
+
+      //  console.log(data);
+        
         await page.waitForSelector('body > main > section:nth-child(3) > form > fieldset.valid');
         await page.type('INPUT[name=vehicle]', licencePlate)
         await page.type('INPUT[name=tenant]', apartmentNumber)
         await page.type('INPUT[name=token]', passcode)
 
         await page.select('select[name="startDate"]', startDate)
-        await page.select('select[name="valid.min.time"]', startTime);
+        await page.waitForTimeout(200)
+        await page.select('select[name="valid.min.time"]', startTime); 
 
-        //check if selector exists or not 
-        const durationSelector = await page.$('INPUT[name=duration]');
+        // await page.waitForTimeout(200)
+        // await page.waitForSelector('body > main > section:nth-child(3) > form > fieldset.valid > fieldset.valid.duration > label > select', {
+        //     visible: true,
+        //   });
+        //  await page.select('body > main > section:nth-child(3) > form > fieldset.valid > fieldset.valid.duration > label > select', parkingDuration)
 
-        if (durationSelector) {
-            await page.select('select[name=duration]', parkingDuration)
-        }
+        // const durationSelector = await page.$('body > main > section:nth-child(3) > form > fieldset.valid > fieldset.valid.duration > label > select ');
+        // if (durationSelector) {
+        // }
 
-
-     //   await page.select('select[name="duration"]', parkingDuration);
 
         //await page.type('INPUT[type=email]', ('start date : ' + startDate + ' start time : ' + startTime))
 
 
+      //  const data = await page.evaluate(() => document.querySelector('fieldset .valid.duration > ul > li:nth-child(1) > label > input[type=radio]').outerHTML);
+     //   console.log(data);
 
-
+await page.click('fieldset .valid.duration > ul > li:nth-child(1) > label > input[type=radio]')
         await page.type('INPUT[type=email]', contactEmail)
         await page.type('INPUT[type=tel]', contactPhone)
 
-       // await page.waitForTimeout(30000);
-
-        await page.click('BUTTON[type=submit')
+        await page.click('BUTTON[type=submit]')
 
 
-         //check if chomre alert appeared     
+        //check if chomre alert appeared     
         page.on('dialog', async dialog => {
             console.log(dialog.message());
             console.log('a dialog appeared !')
             UpdateRegistration(id, 'registration_error', dialog.message())
-            UpdateRegistration(id, 'registration_status', false)
+            UpdateRegistration(id, 'registration_state', 'notregistered')
+            await page.waitForTimeout(5000);
             await dialog.accept();
+            console.log('closing browser');
+            await browser.close();
         });
 
         //check if an alert dialog appeared then log the message 
-        await page.waitForTimeout(3000);
+       await page.waitForTimeout(6000)
 
         if (page.url().startsWith('https://homecomingpreserve.parkingattendant.com/p/')) {
-            console.log('registration success')
-            UpdateRegistration(id, 'registration_status', true)
-               await browser.close();
+            const grabRecord = await page.evaluate(() =>{
+                const record = document.querySelector('body > main > section.permit > figure.permit > dl:nth-child(4) > dd:nth-child(4) > data')
+            return record.innerText;
+            })
+            console.log('registration success RECORD : ', grabRecord)
+            UpdateRegistration(id, 'registration_record', grabRecord)
+
+            UpdateRegistration(id, 'registration_state', 'registered')
+            await page.waitForTimeout(5000);
+            
         }
-        else {
+        if(!page.url().startsWith('https://homecomingpreserve.parkingattendant.com/p/')) {
             console.log('registration failed')
-            UpdateRegistration(id, 'registration_status', false)
-              await browser.close();
+            UpdateRegistration(id, 'registration_state', 'notregistered')
+            
+            await page.waitForTimeout(5000);
+
+           
         }
         //finish and close browser
-          await browser.close();
+        console.log('closing browser');
+        await browser.close();
 
     })();
 }
@@ -174,6 +196,7 @@ const CancelRegistration = (
                 '--enable-webgl',
                 '--window-size=800,800'
             ]
+
         });
 
         const submissionUrl = "https://homecomingpreserve.parkingattendant.com/rtqjn8cnc50jzc1enhsmrk4vjw/services"
@@ -189,13 +212,18 @@ const CancelRegistration = (
         await page.type('INPUT[name=tenant]', apartmentNumber)
         await page.type('INPUT[name=password]', passcode)
 
-        await page.click('body > main > section:nth-child(17) > form > button')
+        // await page.click('body > main > section:nth-child(17) > form > button')
+        //await page.waitForTimeout(3000);
+        const button = await page.$('body > main > section:nth-child(17) > form > button');
+        await button.evaluate(b => b.click());
+
+        //  await page.click('BUTTON[type="submit"]')
 
         page.on('dialog', async dialog => {
             console.log(dialog.message());
             console.log('a dialog appeared !')
             UpdateRegistration(id, 'registration_cancel_error', dialog.message())
-            //UpdateRegistration(id, 'registration_status', false)
+            //UpdateRegistration(id, 'registration_state', 'notregistered')
             await dialog.accept();
         });
 
@@ -204,7 +232,8 @@ const CancelRegistration = (
         await page.waitForTimeout(5000);
         //check if we reached permits page, or close browser
         const elementExists = await page.$('body > main > section:nth-child(17) > article > ul > li > ul:nth-child(4) > li > a');
-        if (!elementExists) { console.log('closing browser') }
+        console.log('closing browser');if (!elementExists) { console.log('closing browser'); 
+        await browser.close(); }
         else {
 
             //wait for details button, click on details of permit
@@ -219,18 +248,26 @@ const CancelRegistration = (
             await page.type('INPUT[id=send-to-email]', contactEmail)
 
             //click cancel button
-            await page.waitForSelector('body > main > section.permit > figure.permit > nav > section > form > button');
+            // await page.waitForSelector('body > main > section.permit > figure.permit > nav > section > form > button');
             // await page.click('body > main > section.permit > figure.permit > nav > section > form > button')
+
+            const button = await page.$('body > main > section.permit > figure.permit > nav > section > form > button');
+            await button.evaluate(b => b.click());
+
+
             UpdateRegistration(id, 'registration_cancel_error', '')
             UpdateRegistration(id, 'registration_error', '')
+            UpdateRegistration(id, 'registration_record', '')
 
-            UpdateRegistration(id, 'registration_status', false)
+            UpdateRegistration(id, 'registration_state', 'notregistered')
 
 
             console.log('registration cancelled, id : ', id)
         }
 
+        await page.waitForTimeout(10000);
 
+        console.log('closing browser');
         await browser.close();
 
 
@@ -268,34 +305,40 @@ const FetchRegistrations = async () => {
         //console.log(registrationsList.length);
         if (registrationsList.length > 0) {
             registrationsList.map((registration) => {
-                var startDatePrepare = new Date(registration.registration_start_date + ' ' + registration.registration_start_time)//.toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
-                var startDate = new Date(startDatePrepare.getFullYear(), startDatePrepare.getMonth(), startDatePrepare.getDate())               
-               
-                var endDatePrepare = new Date(startDate.getTime() + (Number(registration.registration_hours_until_cancel) * 60 * 60 * 1000));
-                var endDate = new Date(endDatePrepare.getFullYear(), endDatePrepare.getMonth(), endDatePrepare.getDate())
+                var startDate = new Date(registration.registration_start_date + ' 21:00 ' );
+                startDate.setHours(0, 0, 0, 0)
 
-  
-                if (registration.registration_status == false) {
+
+                var startTime = new Date(registration.registration_start_date + ' ' + registration.registration_start_time)//.toLocaleTimeString('en-US', {timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false })
+
+                var endTime = new Date(startTime.getTime() + (Number(registration.registration_hours_until_cancel) * 60 * 60 * 1000));
+
+                var cancelTimePassed = new Date().setMonth(4, 4, 4) >= endTime.setMonth(4, 4, 4)
+
+
+                if (registration.registration_state == 'notregistered') {
                     var minuteTolerance = 10;
-                    var isSameHour = Number(new Date().toLocaleString('en-US', { timeZone: "America/Los_Angeles", hour: 'numeric', hour12: true }).substring(0, 2)) == Number(registration.registration_start_time.split(':')[0]);
-                    var isSameMinute = (Number(registration.registration_start_time.split(':')[1].substring(0, 2)) >= Number(String(new Date().getMinutes()).padStart(2, '0')) - minuteTolerance) && (Number(registration.registration_start_time.split(':')[1].substring(0, 2)) <= Number(String(new Date().getMinutes()).padStart(2, '0')) + minuteTolerance);
+                    var isSameHour = Number(new Date().toLocaleString('en-US', { timeZone: "America/Los_Angeles", hour: 'numeric', hour12: false }).substring(0, 2)) == Number(registration.registration_start_time.split(':')[0]);
+                    var isSameMinute =  (Number(registration.registration_start_time.split(':')[1].substring(0, 2)) <= Number(String(new Date().getMinutes()).padStart(2, '0')) + minuteTolerance) && (Number(registration.registration_start_time.split(':')[1].substring(0, 2)) >= Number(String(new Date().getMinutes()).padStart(2, '0')) - minuteTolerance);
                     var isActiveDay = registration.registration_active_days.includes(new Date().toLocaleString('en-us', { timeZone: "America/Los_Angeles", weekday: 'long' }))
-                    var isStartingDayPassed = startDate <= new Date();
+                    var isStartingDayPassed = startDate <= new Date().setHours(0, 0, 0, 0);
+
                     //check is star date has passed or not 
                     //compare startDate and endDate without time 
 
 
-                    
-                    var eligible = isActiveDay && isSameHour && isSameMinute && isStartingDayPassed
 
-                    
+                    var eligible = (isActiveDay && isSameHour && isSameMinute && isStartingDayPassed && (registration.registration_state == 'notregistered'))
+
+
+                    var done = false;
                     console.log(`
                     `)
-                    console.log('---------- FALSE STATUS, ID : ', registration.registration_id, ' --------------')
+                    console.log('---------- FALSE STATE, ID : ', registration.registration_id, ' --------------')
 
 
-                    if (eligible) {
-                          console.log('eligible for registration,  id :', registration.registration_id);
+                    if (eligible && !done) {
+                        console.log('eligible for registration,  id :', registration.registration_id);
                         RegisterVehicle(
                             registration.registration_id,
                             registration.registration_licence_plate,
@@ -308,24 +351,25 @@ const FetchRegistrations = async () => {
                             registration.registration_contact_phone,
 
                         )
+                        done = true
                     } else {
                         console.log('not eligible for registration, because of : ',
-                            ((!isActiveDay) ? '- not same day -' : ''),
+                            ((!isActiveDay) ? '- not an active day -' : ''),
                             ((!isSameHour) ? ('- not same hour -') : ''),
                             ((!isSameMinute) ? '- not same minute -' : ''),
                             ((!isStartingDayPassed) ? '- start date is not reached -' : ''));
                     }
-                    if (!isSameHour) {
-                        console.log('Registration Hour : ' + Number(registration.registration_start_time.split(':')[0]) + ' Current : ' + Number(new Date().toLocaleString('en-US', { hour: 'numeric', hour12: true, timeZone: "America/Los_Angeles" }).substring(0, 2)))
-                    }
-                    if (!isSameMinute) {
-                        console.log('Registration Minute : ' + Number(registration.registration_start_time.split(':')[1].substring(0, 2)) + ' Current : ' + Number(String(new Date().getMinutes()).padStart(2, '0')))
-                    }
- 
-                    if (!isStartingDayPassed) {
-                        console.log('Registration Start Date : ' + startDate.toLocaleString("en-US", { timeZone: "America/Los_Angeles" }) + ' Current ' + new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" }))
-                    }
-                    if (!isActiveDay) { console.log('Registration Active Days : ' + registration.registration_active_days) }
+                    console.log('♠ Registration Hour : ' + Number(registration.registration_start_time.split(':')[0]) + ' Current : ' + Number(new Date().toLocaleString('en-US', { hour: 'numeric', hour12: false, timeZone: "America/Los_Angeles" }).substring(0, 2)))
+                    console.log('♠ Registration Minute : ' + Number(registration.registration_start_time.split(':')[1].substring(0, 2)) + ' Current : ' + Number(String(new Date().getMinutes()).padStart(2, '0')))
+                    console.log('♠ Registration Start Date : ' + startDate.toLocaleDateString("en-US", { timeZone: "America/Los_Angeles" })+  ' Current ' + new Date().toLocaleDateString("en-US", { timeZone: "America/Los_Angeles" }))
+                    console.log('♠ Registration Start Time : ' + startTime.toLocaleTimeString('en-US', { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }) + '  ('+registration.registration_hours_until_cancel+' Hours)')
+                    console.log('♠ Registration End Time : ' + endTime.toLocaleString("en-US", { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }) + ' Current : ' + new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }))
+
+                    console.log('♠ Registration Active Days : ' + registration.registration_active_days)
+
+                    console.log('♠ Cancel Time Passed : ' + cancelTimePassed)
+
+
 
                     console.log(`
                     `)
@@ -333,17 +377,21 @@ const FetchRegistrations = async () => {
                 }
 
                 //registrations that are registered and waiting for cancellation
-                else if (registration.registration_status == true) {
+                else if (registration.registration_state == 'registered') {
                     console.log(`
                     `)
 
-                    console.log('---------- TRUE STATUS, ID : ', registration.registration_id, '  --------------')
+                    console.log('---------- TRUE STATE, ID : ', registration.registration_id, '  --------------')
 
                     console.log('registration already done, id : ', registration.registration_id)
+                    console.log('♠ Registration Start Time : ' + startTime.toLocaleTimeString('en-US', { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }))
+                    console.log('♠ Registration End Time : ' + endTime.toLocaleString("en-US", { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }) + ' Current : ' + new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles", hour: 'numeric', minute: 'numeric', hour12: false }))
+
+                    console.log('♠ Cancel Time Passed : ' + cancelTimePassed)
 
 
                     //check if endDate has passed or not
-                    if (endDate <= startDate) {
+                    if (cancelTimePassed) {
                         console.log('registration is eligible for cancellation, id : ', registration.registration_id);
                         // return ;
                         CancelRegistration(
@@ -373,18 +421,18 @@ const FetchRegistrations = async () => {
 
 //FetchRegistrations()
 
-const task = cron.schedule('* */5 * * * *', async () => {
-    await FetchRegistrations();
-    event.emit('JOB COMPLETED');
-}, {
-    timezone: "America/Los_Angeles"
-});
+ const task = cron.schedule('* */5 * * * *', async () => {
+     await FetchRegistrations();
+     event.emit('JOB COMPLETED');
+ }, {
+     timezone: "America/Los_Angeles"
+ });
 
 event.on('JOB COMPLETED', () => {
 
     console.log('Job done!');
     task.stop();
-    setTimeout(() => { task.start() }, 60000);
+    setTimeout(() => { task.start() }, 200000);
 });
 
 app.listen(8080, () => {
